7a11d208a3f8910c9a2311776adf19315f12eac5
unknown <ayavillaruel185gmail.com>
2025-03-24 16:43:13 +0800

    Activity

---
+++ Activities/Functional- Activities/script.js
@@ -0,0 +1,12 @@
+function whatShallWear(temp) {
+    if (temp < 60) {
+        console.log("Wear a jacket");
+    } else if (temp < 70) {
+        console.log("Wear a t-shirt");
+    } else {
+        console.log("Wear a Sweater");
+    }
+}
+whatShallWear(80)
+whatShallWear(60)
+whatShallWear(50)
